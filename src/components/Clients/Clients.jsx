904f751e9025e3ac000006c2a57079eea210b4f8
Amir Sultan <as5969875@gmail.com>
2023-09-09 13:30:51 +0500

    portfolio

---
+++ src/components/Clients/Clients.jsx
@@ -0,0 +1,188 @@
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+import ClientSlider from "./ClientSlider";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { Slide } from "react-awesome-reveal";
+
+let clients = [
+  {
+    name: "Muneer Khair",
+    position: "Mk mobile detaling manigar",
+    img_url:
+      "https://i.ibb.co/fpCRSBn/Whats-App-Image-2023-04-21-at-16-35-11.jpg",
+    stars: 5,
+    disc: `I am thrilled with the outstanding service I received from Ghulam Abbas. From start to finish, the team demonstrated exceptional professionalism and expertise. They were attentive to my needs, provided regular updates, and delivered the final product ahead of schedule. The quality exceeded my expectations, and I highly recommend their services to anyone in need.`,
+  },
+  {
+    name: "Shamsuddin Malik ",
+    position: "web developer",
+    img_url: "/clint.jpg",
+    stars: 5,
+    disc: `Highly satisfied with the services provided by [Ghulam Abbas]. Professionalism, expertise, and attention to detail were top-notch. Exceeded expectations, delivering a high-quality product within agreed timeframe. Clear, prompt communication and valuable insights. Exceptional craftsmanship and attention to detail. Highly recommend!"`,
+  },
+  {
+    name: "John Michel",
+    position: "web developer",
+    img_url:
+      "https://t4.ftcdn.net/jpg/02/90/27/39/360_F_290273933_ukYZjDv8nqgpOBcBUo5CQyFcxAzYlZRW.jpg",
+    stars: 5,
+    disc: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Temporibus consequuntur dolores labore natus similique nemo doloribus cum accusantium adipisci maiores.`,
+  },
+  {
+    name: "John Michel",
+    position: "web developer",
+    img_url:
+      "https://t4.ftcdn.net/jpg/02/90/27/39/360_F_290273933_ukYZjDv8nqgpOBcBUo5CQyFcxAzYlZRW.jpg",
+    stars: 5,
+    disc: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Temporibus consequuntur dolores labore natus similique nemo doloribus cum accusantium adipisci maiores.`,
+  },
+];
+var settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 530,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
+const Clients = () => {
+  const arrowRef = useRef(null);
+  let clientDisc = "";
+  clientDisc = clients.map((item, i) => <ClientSlider item={item} key={i} />);
+  return (
+    <Container id="client">
+      <Slide direction="left">
+        <span className="green">testimonials</span>
+        <h1>what clients say</h1>
+      </Slide>
+      <Testimonials>
+        <Slider ref={arrowRef} {...settings}>
+          {clientDisc}
+        </Slider>
+        <Buttons>
+          <button onClick={() => arrowRef.current.slickPrev()}>
+            <IoIosArrowBack />
+          </button>
+          <button onClick={() => arrowRef.current.slickNext()}>
+            <IoIosArrowForward />
+          </button>
+        </Buttons>
+      </Testimonials>
+    </Container>
+  );
+};
+
+export default Clients;
+
+const Container = styled.div`
+  width: 80%;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 4rem 0;
+
+  @media (max-width: 840px) {
+    width: 90%;
+  }
+
+  span {
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+
+  h1 {
+    padding-top: 1rem;
+    text-transform: capitalize;
+  }
+
+  .slick-list,
+  .slick-slider,
+  .slick-track {
+    padding: 0;
+  }
+
+  .slick-dots {
+    text-align: left;
+    margin-left: 1rem;
+  }
+
+  .slick-dots li button:before {
+    content: "";
+  }
+
+  .slick-dots li button {
+    width: 9px;
+    height: 4px;
+    background: linear-gradient(
+      159deg,
+      rgb(45, 45, 58) 0%,
+      rgb(43, 43, 53) 100%
+    );
+    padding: 0.1rem;
+    margin-top: 1rem;
+    transition: all 400ms ease-in-out;
+    border-radius: 50px;
+  }
+
+  .slick-dots li.slick-active button {
+    background: #01be96;
+    width: 15px;
+  }
+
+  .slick-dots li {
+    margin: 0;
+  }
+`;
+
+const Testimonials = styled.div`
+  margin-top: 2rem;
+  position: relative;
+`;
+const Buttons = styled.div`
+  position: absolute;
+  right: 0.7rem;
+  bottom: -2rem;
+
+  button {
+    background-color: transparent;
+    margin-left: 0.5rem;
+    border: none;
+    color: #01be96;
+    cursor: pointer;
+    font-size: 1.1rem;
+  }
+
+  @media (max-width: 530px) {
+    display: none;
+  }
+`;
